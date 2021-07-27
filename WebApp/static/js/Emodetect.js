0ee9f4dd2f81160cdd6d8c6defcb503cdb25721a
wisdomtooth546 <premoommen@hotmail.com>
2021-07-27 22:40:25 +1000

    Minor tweaks and major fer fix

--- WebApp/static/js/Emodetect.js
+++ WebApp/static/js/Emodetect.js
@@ -44,15 +44,15 @@ function startObjectDetection() {
     //Set canvas sizes based on input video
     drawCanvas.width = v.videoWidth;
     drawCanvas.height = v.videoHeight;
-    imageCanvas.width = v.videoWidth;
-    imageCanvas.height = v.videoHeight;
+    imageCanvas.width = uploadWidth;
+    imageCanvas.height = uploadWidth * (v.videoHeight/v.videoWidth);
     console.log(window.innerWidth, window.innerHeight);
     //Some styles for the drawcanvas
     drawCtx.lineWidth = "4";
     drawCtx.strokeStyle = "blue";
     drawCtx.font = "20px Verdana";
-    drawCtx.fillStyle = "green";
-    imageCtx.drawImage(v, 0, 0, v.videoWidth, v.videoHeight, (imageCanvas.clientWidth-v.videoWidth)/4, 0, v.videoWidth, v.videoHeight);
+    drawCtx.fillStyle = "yellow";
+    imageCtx.drawImage(v, 0, 0, v.videoWidth, v.videoHeight, 0, 0, uploadWidth, uploadWidth * (v.videoWidth/v.videoHeight));
     imageCanvas.toBlob(postFile, 'image/jpeg');
    
  
@@ -90,7 +90,7 @@ function drawBoxes(object) {
     
     //filter out objects that contain a class_name and then draw boxes and labels on each
     
-    let x = (object.x);
+    let x = object.x;
     let y = object.y;
     let width = (object.w) + x;
     let height = (object.h)+ y;
@@ -99,55 +99,9 @@ function drawBoxes(object) {
             x = drawCanvas.width - (x + width)
         }
     //+ Math.round(object.score * 100, 1) + "%"
-    drawCtx.fillText(object.class_name, (imageCanvas.clientWidth-v.videoWidth)/4+x + 5, y + 20);
-    drawCtx.strokeRect((imageCanvas.clientWidth-v.videoWidth)/4+x, y, width, height);
+    drawCtx.fillText(object.class_name, x + 5, y - 70);
+    drawCtx.strokeRect(x-100, y-50, width-10, height);
  
     
 }
 
-function sendImageFromCanvas() {
- 
-    imageCtx.drawImage(v, 0, 0, v.videoWidth, v.videoHeight, 0, 0, uploadWidth, uploadWidth * (v.videoHeight / v.videoWidth));
- 
-    let imageChanged = imageChange(imageCtx, 0.1);
-    let enoughTime = (new Date() - lastFrameTime) > updateInterval;
- 
-    if (imageChanged && enoughTime) {
-        imageCanvas.toBlob(postFile, 'image/jpeg');
-        lastFrameTime = new Date();
-    }
-    else {
-        setTimeout(sendImageFromCanvas, updateInterval);
-    }
-}
-
-function imageChange(sourceCtx, changeThreshold) {
- 
-    let changedPixels = 0;
-    const threshold = changeThreshold * sourceCtx.canvas.width * sourceCtx.canvas.height;   //the number of pixes that change change
- 
-    let currentFrame = sourceCtx.getImageData(0, 0, sourceCtx.canvas.width, sourceCtx.canvas.height).data;
- 
-    //handle the first frame
-    if (lastFrameData === null) {
-        lastFrameData = currentFrame;
-        return true;
-    }
- 
-    //look for the number of pixels that changed
-    for (let i = 0; i < currentFrame.length; i += 4) {
-        let lastPixelValue = lastFrameData[i] + lastFrameData[i + 1] + lastFrameData[i + 2];
-        let currentPixelValue = currentFrame[i] + currentFrame[i + 1] + currentFrame[i + 2];
- 
-        //see if the change in the current and last pixel is greater than 10; 0 was too sensitive
-        if (Math.abs(lastPixelValue - currentPixelValue) > (10)) {
-            changedPixels++
-        }
-    }
- 
-    //console.log("current frame hits: " + hits);
-    lastFrameData = currentFrame;
- 
-    return (changedPixels > threshold);
- 
-}
